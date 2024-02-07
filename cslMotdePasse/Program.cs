7b451ddd42e232f67337a4af850d2b2121595db7
igoudjil_j <igoudjil_j@D202-022024-08>
2024-02-07 10:17:50 +0100

    Ajoutez des fichiers projet.

---
+++ cslMotdePasse/Program.cs
@@ -0,0 +1,11 @@
+﻿using libMotdePasse;
+using System.Globalization;
+Console.WriteLine("saisir le login");
+string a= Console.ReadLine();
+Console.WriteLine("saisir le mdp");
+string b= Console.ReadLine();
+MotdePasse conn1=new MotdePasse(a,b);
+
+Console.WriteLine("resaisir le mdp");
+string c = Console.ReadLine();
+Console.WriteLine(conn1.Verifier(c));
---
+++ libMotdePasse/MotdePasse.cs
@@ -0,0 +1,40 @@
+﻿namespace libMotdePasse
+{
+    public class MotdePasse
+    {
+
+        private string Login;
+        private string mdp;
+
+        public MotdePasse(string a, string b)
+        {
+            this.Login = a;
+            this.mdp = b;
+        }
+        public bool Verifier(string password)
+        {
+            bool flag = false;
+            if(this.mdp == password)
+            {
+            flag = true;
+        }
+           return flag;
+        }
+        public bool Longeur()
+        {
+            bool flag= false;
+            if(this.mdp.Length > 11)
+            {
+                flag = true;
+            }
+                return flag;
+        }
+        public bool EstNumeric()
+        {
+            bool flag= false;
+            if (this.mdp.Char.IsNumber
+        }
+    }
+}
+
+ 
